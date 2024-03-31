0ea354179c22b69b817b1b2bc1b77b2cd3bf2586
Abdul <abduldevelop@gmail.com>
2024-03-31 09:20:55 +0530

    fix a typo

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,5 @@
 import Home from "./home/Home";
 import Video from "./video/Video";
 import SignIn from "./signin/SignIn";
-import SignUp from "./singup/SIgnUp";
+import SignUp from "./singup/SignUp";
 export { Home, Video, SignIn, SignUp };
